0dfb54485e311fc13dcf10501a2264ab1ff86c19
lee-jong <44047449+lee-jong@users.noreply.github.com>
2019-09-30 11:17:53 +0900

    login/ vaild api 수정 및 추가 완료

--- action/login.js
+++ action/login.js
@@ -6,17 +6,26 @@ const fs = require('fs')
 const jwt = require('jsonwebtoken')
 
 module.exports = app => {
-    app.post('/login', (req, res) => {
-        let id = 'id'
-        let pw = 'pw'
-
+    app.post('/getToken', (req, res) => {
         if (!req.body.id || !req.body.pw) return res.json({ status: 402, message: 'go wrong request' })
+        let sql = `select * from user where id = '${req.body.id}'`
+        connection.query(sql, (err, result) => {
+            if (err) return res.json({ message: 'err', err })
+            if (result[0].pw !== req.body.pw) return res.json({ status: 402, message: 'go wrong request pw' })
 
-        if (id === req.body.id && pw.req.body.pw) {
-            let token = jwt.sign({ id: req.body.id }, 'secret', { expiresIn: '24h' })
-            return res.json({ status: 200, message: 'get token success', token })
-        } else {
-            return res.json({ status: 402, message: 'do not match id with pw' })
-        }
+            let token = jwt.sign({ id: req.body.id }, 'secret', { expiresIn: '12h' })
+            return res.json({ status: 200, message: 'login success', token })
+        })
+    })
+
+    app.post('/vaildToken', (req, res) => {
+        let token = req.body.token
+        jwt.verify(token, 'secret', (err, decoded) => {
+            if (err) {
+                return res.json({ status: 401, message: 'Token is not valid' })
+            } else {
+                return res.json({ status: 200, message: 'Token is valid', decoded })
+            }
+        })
     })
 }
